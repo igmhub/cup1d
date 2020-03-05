f29c990f17ec210dd29edd118808d2b98c72ac3e
Andreu Font-Ribera <font.andreu@gmail.com>
2020-03-05 00:39:13 -0700

    setup setup

---
+++ py/cup1d/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+"""
+=======
+cup1d
+=======
+Cosmology using P1D - small scale clustering of the Lyman alpha forest.
+"""
+
+from ._version import __version__
---
+++ py/cup1d/p1d_archive.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def test_function():
+    print(3)
