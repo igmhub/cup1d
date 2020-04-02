2143a98a6f4d3b6c37e24498c01a6142d1526f6d
Andreu Font-Ribera <font.andreu@gmail.com>
2020-04-02 18:58:08 +0200

    added code for cosmology and user interface

---
+++ py/cosmology/__init__.py
@@ -0,0 +1,14 @@
+import sys
+
+"""
+=======
+cup1d -- cosmology
+=======
+
+Module containing cosmological functions used in cup1d.
+
+It uses CAMB to compute most functions, but we could also use CLASS if needed.
+
+"""
+
+from ._version import __version__
