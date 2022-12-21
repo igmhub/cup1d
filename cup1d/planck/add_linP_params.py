356125a880f468d9d0a989a08b4d9ab4b9bef650
Andreu Font <afont@ifae.es>
2022-12-21 17:58:58 +0100

    updated add_linP_params module, to use LaCE cosmo instead

--- cup1d/planck/add_linP_params.py
+++ cup1d/planck/add_linP_params.py
@@ -1,18 +1,18 @@
-from cup1d.cosmology import camb_cosmo
-from cup1d.cosmology import fit_linP
+from lace.cosmo import camb_cosmo
+from lace.cosmo import fit_linP
 
 def get_linP_params(params,z_star=3.0,kp_kms=0.009,z_evol=False,verbose=False):
     """Given point in getdist MCMC chain, compute linear power parameters.
-        - z_star, kp_kms set the pivot point.
-        - z_evol=True will also compute (f_star,g_star) for z evolution. """
+        - z_star, kp_kms set the pivot point """
 
     # create CAMB cosmology object from input params dictionary
-    cosmo=camb_cosmo.get_cosmology(params)
+    cosmo=camb_cosmo.get_cosmology_from_dictionary(params)
     if verbose: 
-        camb_cosmo.print_info(cosmo,simulation=True)
+        camb_cosmo.print_info(cosmo)
+
     # compute linear power and fit power law at pivot point
-    linP_model=fit_linP.LinearPowerModel(cosmo=cosmo,z_star=z_star,
-                                    k_units='kms',kp=kp_kms,z_evol=z_evol)
-    linP_params=linP_model.get_params(z_evol=z_evol)
+    linP_params=fit_linP.parameterize_cosmology_kms(cosmo,camb_results=None,
+                    z_star=z_star,kp_kms=kp_kms)
+
     return linP_params
 
