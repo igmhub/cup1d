98fec8d955a77515f4e6f55b6ea4d6fe41165757
Andreu Font <font.andreu@gmail.com>
2022-12-22 06:38:38 -0800

    updated notebook to compute linP params for Planck chain

--- cup1d/planck/add_linP_params.py
+++ cup1d/planck/add_linP_params.py
@@ -1,7 +1,7 @@
 from lace.cosmo import camb_cosmo
 from lace.cosmo import fit_linP
 
-def get_linP_params(params,z_star=3.0,kp_kms=0.009,z_evol=False,verbose=False):
+def get_linP_params(params,z_star=3.0,kp_kms=0.009,verbose=False):
     """Given point in getdist MCMC chain, compute linear power parameters.
         - z_star, kp_kms set the pivot point """
 
