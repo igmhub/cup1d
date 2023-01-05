2a1d5e71ebbe678b852e9fba604601672c516466
Andreu Font <font.andreu@gmail.com>
2023-01-05 01:40:52 -0800

    mock_data can be set from label

--- cup1d/data/mock_data.py
+++ cup1d/data/mock_data.py
@@ -1,16 +1,36 @@
+from lace.emulator import gp_emulator
 from cup1d.data import base_p1d_data
+from cup1d.data import data_Chabanier2019
+from cup1d.data import data_Karacayli2022
+from cup1d.data import data_QMLE_Ohio
+from cup1d.likelihood import lya_theory
+
 
 class Mock_P1D(base_p1d_data.BaseDataP1D):
     """ Class to generate a mock P1D from another P1D object and a theory"""
 
-    def __init__(self,data,theory):
+    def __init__(self,emulator=None,data_label="Chabanier2019",
+                    zmin=2.0,zmax=4.5):
         """ Copy data and replace P1D signal using theory"""
 
-        # keep theory in case you need it later
-        self.theory=theory
+        # load original data
+        self.data_label=data_label
+        if data_label=="Chabanier2019":
+            data=data_Chabanier2019.P1D_Chabanier2019(zmin=zmin,zmax=zmax)
+        elif data_label=="QMLE_Ohio":
+            data=data_QMLE_Ohio.P1D_QMLE_Ohio(zmin=zmin,zmax=zmax)
+        elif data_label=="Karacayli2022":
+            data=data_Karacayli2022.P1D_Karacayli2022(zmin=zmin,zmax=zmax)
+        else:
+            raise ValueError("Unknown data_label",data_label)
+
+        # setup theory
+        if emulator is None:
+            emulator=gp_emulator.GPEmulator()
+        self.theory=lya_theory.Theory(zs=data.z,emulator=emulator)
 
         # evaluate theory at k_kms, for all redshifts
-        emu_p1d_kms=theory.get_p1d_kms(data.k_kms)
+        emu_p1d_kms=self.theory.get_p1d_kms(data.k_kms)
 
         # at each z, update value of p1d
         Pk_kms=data.Pk_kms.copy()
