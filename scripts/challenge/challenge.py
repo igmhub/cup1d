0daa90a7f4d15768749d709aebfe0f20cfbee25d
jchavesmontero <jchaves@ifae.es>
2025-03-03 10:33:59 +0100

    a

--- scripts/challenge/challenge.py
+++ scripts/challenge/challenge.py
@@ -66,14 +66,14 @@ def main():
     print(path_in_challenge)
     print(path_out_challenge)
 
-    search = os.path.join(
-        path_in_challenge, "*" + version + "_nonoise_fiducial*"
-    )
+    # search = os.path.join(
+    #     path_in_challenge, "*" + version + "_nonoise_fiducial*"
+    # )
     # search = os.path.join(path_in_challenge, "*CGAN_4096_base*")
     # search = os.path.join(path_in_challenge, "*CGAN_4096_val*")
     # search = os.path.join(path_in_challenge, "*cosmo_grid_3*")
     # files = np.sort(glob.glob(path_in_challenge + "*bar_ic*.fits"))
-    # search = os.path.join(path_in_challenge, "*Sherwood_2048_40*")
+    search = os.path.join(path_in_challenge, "*Sherwood_2048_40*")
     # search = os.path.join(path_in_challenge, "*ACCEL2_6144_160*")
 
     # print(search)
