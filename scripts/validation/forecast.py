3612ff89211745488e478ab126f57fcf48ac43e2
jchavesmontero <jchaves@ifae.es>
2025-04-03 16:30:04 +0200

    a

--- scripts/validation/forecast.py
+++ scripts/validation/forecast.py
@@ -50,7 +50,8 @@ def main():
     # emulator_label = "CH24_nyx_gp"
     # emulator_label = "CH24"
     # emulator_label = "CH24_NYX"
-    emu_cov_factor = 1.0
+    emu_cov_factor = None
+    # emu_cov_factor = 1.0
     emu_cov_type = "diagonal"
     # emu_cov_type = "block"
     # emu_cov_type = "full"
