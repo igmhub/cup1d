31a178d213784af652d398a1d9ce759ff17b226c
jchavesmontero <jchaves@ifae.es>
2025-07-26 00:10:03 +0200

    a

--- cup1d/likelihood/fitter.py
+++ cup1d/likelihood/fitter.py
@@ -419,7 +419,7 @@ class Fitter(object):
             print("diff chi2", chi2_1 - chi2_2, flush=True)
             if res.success == True:
                 keep = False
-            elif (chi2_1 - chi2_2) < 0.5:
+            elif (chi2_1 - chi2_2) < 0.25:
                 keep = False
             else:
                 keep = True
@@ -938,7 +938,21 @@ class Fitter(object):
         dict_out["like"] = {}
         # dict_out["like"]["cosmo_fid_label"] = self.like.fid
         dict_out["like"]["emu_cov_factor"] = self.like.emu_cov_factor
-        dict_out["like"]["free_params"] = self.like.free_param_names
+        dict_out["like"]["free_param_names"] = self.like.free_param_names
+        dict_out["like"]["free_params"] = self.like.free_params
+        for par in self.like.free_params:
+            dict_out["like"]["free_params"][par.name] = {}
+            dict_out["like"]["free_params"][par.name]["value"] = par.value
+            dict_out["like"]["free_params"][par.name][
+                "min_value"
+            ] = par.min_value
+            dict_out["like"]["free_params"][par.name][
+                "max_value"
+            ] = par.max_value
+            dict_out["like"]["free_params"][par.name]["fixed"] = par.fixed
+            dict_out["like"]["free_params"][par.name][
+                "Gauss_priors_width"
+            ] = par.Gauss_priors_width
 
         # SAMPLER
         if save_chains:
