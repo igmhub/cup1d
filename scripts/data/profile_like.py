81f84b8e20d35eaef320a563e7f5ad3e3902855a
jchavesmontero <jchaves@ifae.es>
2025-08-21 10:48:43 +0200

    a

--- scripts/data/profile_like.py
+++ scripts/data/profile_like.py
@@ -9,10 +9,8 @@ from cup1d.utils.utils import get_path_repo
 
 
 def main():
-    type_minimizer = "NM"
-    # type_minimizer = "DA"
-    emu = "mpg"
-    # emu = "nyx"
+    # emu = "mpg"
+    emu = "nyx"
 
     # baseline
     fit_type = "global_opt"
@@ -52,7 +50,7 @@ def main():
     # sigma_cosmo = {"n_star": 0.01}
     # out_folder = os.path.join(args.out_folder, "prof_nstar")
 
-    pip.run_profile(sigma_cosmo, nelem=10, type_minimizer=type_minimizer)
+    pip.run_profile(sigma_cosmo, nelem=10, type_minimizer="NM")
 
 
 if __name__ == "__main__":
