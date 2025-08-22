65f8eb4ec58591373880a9f52d875406ff2d3dd7
jchavesmontero <jchaves@ifae.es>
2025-08-22 15:35:05 +0200

    a

--- scripts/data/profile_like.py
+++ scripts/data/profile_like.py
@@ -11,22 +11,22 @@ from cup1d.utils.utils import get_path_repo
 def main():
     # emu = "mpg"
     emu = "nyx"
-    # data_label = "DESIY1_QMLE3"
+    data_label = "DESIY1_QMLE3"
     # data_label = "DESIY1_QMLE"
-    data_label = "DESIY1_FFT_dir"
+    # data_label = "DESIY1_FFT_dir"
     # data_label = "DESIY1_FFT"
 
-    # prof_type = "prof_2d"
-    # nsig = 10
-    # nelem = 10
-    # mle_cosmo_cen = None
+    prof_type = "prof_2d"
+    nsig = 10
+    nelem = 10
+    mle_cosmo_cen = None
 
-    prof_type = "prof_2d_deep2"
-    nsig = 5
-    nelem = 30
+    # prof_type = "prof_2d_deep2"
+    # nsig = 5
+    # nelem = 30
     # mle_cosmo_cen = {"Delta2_star": 0.30836784, "n_star": -2.27185649} # nyx qmle3
     # mle_cosmo_cen = {"Delta2_star": 0.26, "n_star": -2.26}  # nyx qmle
-    mle_cosmo_cen = {"Delta2_star": 0.33, "n_star": -2.255}  # nyx fft_dir
+    # mle_cosmo_cen = {"Delta2_star": 0.33, "n_star": -2.255}  # nyx fft_dir
     # mle_cosmo_cen = {"Delta2_star": 0.44, "n_star": -2.26}  # mpg qmle3
     # mle_cosmo_cen = {"Delta2_star": 0.42, "n_star": -2.28}  # mpg qmle
 
