5a16ad0577575a0eaffd99cd1e7baccb1ac8beb8
jchavesmontero <jchaves@ifae.es>
2025-08-24 00:53:29 +0200

    a

--- scripts/data/profile_like.py
+++ scripts/data/profile_like.py
@@ -13,8 +13,8 @@ def main():
     # emu = "nyx"
     # data_label = "DESIY1_QMLE3"
     # data_label = "DESIY1_QMLE"
-    data_label = "DESIY1_FFT_dir"
-    # data_label = "DESIY1_FFT"
+    # data_label = "DESIY1_FFT_dir"
+    data_label = "DESIY1_FFT"
 
     prof_type = "prof_2d"
     nsig = 10
@@ -24,13 +24,14 @@ def main():
     # prof_type = "prof_2d_deep"
     # nsig = 5
     # nelem = 30
-    # mle_cosmo_cen = {"Delta2_star": 0.36, "n_star": -2.26}  # nyx qmle3
-    # mle_cosmo_cen = {"Delta2_star": 0.42, "n_star": -2.28}  # mpg qmle3
-    # mle_cosmo_cen = {"Delta2_star": 0.42, "n_star": -2.22}  # mpg qmle
+    # mle_cosmo_cen = {"Delta2_star": 0.36, "n_star": -2.26}  # nyx qmle3 done
+    # mle_cosmo_cen = {"Delta2_star": 0.42, "n_star": -2.28}  # mpg qmle3 done
+    # mle_cosmo_cen = {"Delta2_star": 0.42, "n_star": -2.22}  # mpg qmle done
 
     # old
-    # mle_cosmo_cen = {"Delta2_star": 0.26, "n_star": -2.26}  # nyx qmle
-    # mle_cosmo_cen = {"Delta2_star": 0.33, "n_star": -2.255}  # nyx fft_dir
+    # mle_cosmo_cen = {"Delta2_star": 0.26, "n_star": -2.26}  # nyx qmle tbd
+    # mle_cosmo_cen = {"Delta2_star": 0.33, "n_star": -2.255}  # nyx fft_dir launch_shallow
+    # mle_cosmo_cen = {"Delta2_star": 0.33, "n_star": -2.255}  # nyx fft launch_shallow
 
     # baseline
     fit_type = "global_opt"
