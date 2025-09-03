489712fc1f5b29a724d4636391a801b9fcf1a65d
jchavesmontero <jchaves@ifae.es>
2025-09-03 10:19:36 +0200

    update notebooks

--- notebooks/planck/DESI_lya_importance.py
+++ notebooks/planck/DESI_lya_importance.py
@@ -8,9 +8,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.16.1
 #   kernelspec:
-#     display_name: cup1d
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: cup1d
+#     name: python3
 # ---
 
 # %% [markdown]
@@ -30,6 +30,7 @@ from cup1d.planck import add_linP_params
 from cup1d.likelihood import marg_lya_like
 # because of black magic, getdist needs this strange order of imports
 # %matplotlib inline
+from cup1d.utils.utils import get_path_repo
 
 # %% [markdown]
 # ### Read an extended Planck chains and plot linear power parameters
@@ -51,9 +52,98 @@ model='base_mnu'
 planck2018=planck_chains.get_planck_2018(model=model,data=planck_data)
 
 # %%
+
+root_dir=os.path.join(get_path_repo("cup1d"), "data", "planck_linP_chains")
+
+cmb = planck_chains.get_planck_2018(
+    model='base',
+    data='plikHM_TTTEEE_lowl_lowE_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+cmb_nrun = planck_chains.get_planck_2018(
+    model='base_nrun',
+    data='plikHM_TTTEEE_lowl_lowE_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+cmb_nrun_nrunrun = planck_chains.get_planck_2018(
+    model='base_nrun_nrunrun',
+    data='plikHM_TTTEEE_lowl_lowE_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+cmb_mnu = planck_chains.get_planck_2018(
+    model='base_mnu',
+    data='plikHM_TTTEEE_lowl_lowE_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+cmb_omega_k = planck_chains.get_planck_2018(
+    model='base_omegak',
+    data='plikHM_TTTEEE_lowl_lowE_BAO_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+cmb_w_wa = planck_chains.get_planck_2018(
+    model='base_w_wa',
+    data='plikHM_TTTEEE_lowl_lowE_BAO_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
+
+# cmb_nrun_nnu_w_mnu = planck_chains.get_planck_2018(
+#     model='base_nrun_nnu_w_mnu',
+#     data='plikHM_TTTEEE_lowl_lowE_BAO_Riess18_Pantheon18_lensing',
+#     root_dir=root_dir,
+#     linP_tag=None
+# )
+
+# %%
+{'x0': 0.443151934816415,
+ 'y0': -2.2830673979023928,
+ 'a': 0.05671364141407182,
+ 'b': 0.02081226136234671,
+ 'theta': -0.13448802462450818}
+
+# %%
 g = plots.getSinglePlotter(width_inch=6)
-g.plot_2d(planck2018['samples'], ['linP_n_star', 'linP_DL2_star'])
+g.plot_2d(cmb['samples'], ['linP_DL2_star', 'linP_n_star'])
 #g.plot_2d(planck2018['samples'], ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25,0.2,0.5])
+
+true_DL2=0.35
+true_neff=-2.3
+
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3
+thresholds = [2.30, 6.18]
+neff_grid, DL2_grid = np.mgrid[-2.4:-2.2:200j, 0.2:0.65:200j]
+
+chi2_desi = coeff_mult * marg_lya_like.gaussian_chi2(neff_grid,DL2_grid, true_neff, true_DL2, neff_err, DL2_err, r)
+
+CS = plt.contour(DL2_grid, neff_grid, chi2_desi, levels=thresholds[:2], colors='green')
+plt.xlim(0.25, 0.45)
+plt.ylim(-2.35, -2.25)
+
+
+# %%
+def print_err(ell):
+    ver = ell.collections[0].get_paths()[0].vertices
+    xerr = 0.5 * (ver[:,0].max() - ver[:,0].min())
+    yerr = 0.5 * (ver[:,1].max() - ver[:,1].min())
+    print(xerr, yerr)
+
+
+# %%
+print_err(CS)
 
 # %%
 # plot also neutrino mass (for nuLCDM)
@@ -71,59 +161,351 @@ g.triangle_plot(planck2018['samples'],
 # We will generate three fake DESI likelihoods, for different fiducial values, and see how they affect the cosmo params.
 
 # %%
-def gaussian_chi2_mock_DESI(neff,DL2,true_DL2=0.35,true_neff=-2.3):
+def gaussian_chi2_mock_DESI(neff, DL2, true_DL2=0.35, true_neff=-2.3, DL2_err=0.003, neff_err=0.002, r=0.55):
     """Compute Gaussian Delta chi^2 for a particular point(s) (neff,DL2),
     using a mock measurement from DESI (complete made up at this point).
     """
-    # DL2 = k^3 P(k) / (2 pi^2), at z=3
-    DL2_err=0.003
-    # neff = effective slope at kp = 0.009 s/km, i.e., d ln P / dln k
-    neff_err=0.002
-    # correlation coefficient
-    r=0.55
-    return marg_lya_like.gaussian_chi2(neff,DL2,true_neff,true_DL2,neff_err,DL2_err,r)
+    # # DL2 = k^3 P(k) / (2 pi^2), at z=3
+    # DL2_err=0.003
+    # # neff = effective slope at kp = 0.009 s/km, i.e., d ln P / dln k
+    # neff_err=0.002
+    # # correlation coefficient
+    # r=0.55
+    return marg_lya_like.gaussian_chi2(neff, DL2, true_neff, true_DL2, neff_err, DL2_err, r)
 
+
+# %%
+true_DL2=0.35
+true_neff=-2.3
+
+DL2_err = 0.06
+neff_err = 0.02
+r=-0.134
+coeff_mult = 2.3
+
+chi2_desi = marg_lya_like.gaussian_chi2(neff, DL2, true_neff, true_DL2, neff_err, DL2_err, r)
+
+# %%
+# {'x0': 0.443151934816415,
+#  'y0': -2.2830673979023928,
+#  'a': 0.05671364141407182,
+#  'b': 0.02081226136234671,
+#  'theta': -0.13448802462450818}
+
+# %%
+
+# %% [markdown]
+# #### Almost no constraints on As, ns
 
 # %%
 samples_DESI=[]
 true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb
+
+
+# new_samples=cmb_w_wa['samples'].copy()
+# new_samples=cmb_omega_k['samples'].copy()
+# new_samples=cmb_mnu['samples'].copy()
+# new_samples=cmb_nrun['samples'].copy()
+
+
 labels_DESI=[]
-for DL2 in true_DL2:
-    new_samples=planck2018['samples'].copy()
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
     p=new_samples.getParams()
-    new_loglike = 0.5*gaussian_chi2_mock_DESI(p.linP_n_star,p.linP_DL2_star,true_DL2=DL2)
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
     new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
     samples_DESI.append(new_samples)
-    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(DL2))
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
 
-# %%
+
 g = plots.getSubplotPlotter(width_inch=16)
 g.settings.axes_fontsize = 10
 g.settings.legend_fontsize = 14
-g.triangle_plot([planck2018['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
-                ['linP_DL2_star','linP_n_star','linP_alpha_star','linP_f_star','omegam','mnu'],
-                legend_labels=[planck_label,labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','logA','ns'],
+                legend_labels=['Planck 2018 + SDSS',labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+
+# %% [markdown]
+# #### No constraints on w0, wa
 
 # %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_w_wa
+
+
+# new_samples=cmb_w_wa['samples'].copy()
+# new_samples=cmb_omega_k['samples'].copy()
+# new_samples=cmb_mnu['samples'].copy()
+# new_samples=cmb_nrun['samples'].copy()
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
+g = plots.getSubplotPlotter(width_inch=16)
+g.settings.axes_fontsize = 10
+g.settings.legend_fontsize = 14
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','w','wa'],
+                legend_labels=['Planck 2018 + SDSS',labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+
+# %% [markdown]
+# ### Almost no constraints on omega_k
+
+# %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_omega_k
+
+
+# new_samples=cmb_w_wa['samples'].copy()
+# new_samples=cmb_omega_k['samples'].copy()
+# new_samples=cmb_mnu['samples'].copy()
+# new_samples=cmb_nrun['samples'].copy()
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
+g = plots.getSubplotPlotter(width_inch=16)
+g.settings.axes_fontsize = 10
+g.settings.legend_fontsize = 14
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','omegak'],
+                legend_labels=['Planck 2018 + SDSS',labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+
+# %% [markdown]
+# ### Constraints on mnu
+
+# %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_mnu
+
+
+# new_samples=cmb_w_wa['samples'].copy()
+# new_samples=cmb_omega_k['samples'].copy()
+# new_samples=cmb_mnu['samples'].copy()
+# new_samples=cmb_nrun['samples'].copy()
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
 g = plots.getSubplotPlotter(width_inch=8)
 g.settings.axes_fontsize = 12
 g.settings.legend_fontsize = 14
-g.triangle_plot([planck2018['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
-                ['linP_DL2_star','mnu'],
-                legend_labels=[planck_label,labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','mnu'],
+                legend_labels=["Planck 2018",labels_DESI[0],labels_DESI[1],labels_DESI[2]])
 #plt.savefig(plot_label+'.pdf')
 #plt.savefig(plot_label+'.png')
 
-# %%
-print(planck2018['samples'].getInlineLatex('mnu',limit=1))
+print(chain_type['samples'].getInlineLatex('mnu',limit=1))
 print(samples_DESI[0].getInlineLatex('mnu',limit=1))
 print(samples_DESI[1].getInlineLatex('mnu',limit=1))
 print(samples_DESI[2].getInlineLatex('mnu',limit=1))
-
-# %%
-print(planck2018['samples'].getInlineLatex('mnu',limit=2))
+print("")
+print(chain_type['samples'].getInlineLatex('mnu',limit=2))
 print(samples_DESI[0].getInlineLatex('mnu',limit=2))
 print(samples_DESI[1].getInlineLatex('mnu',limit=2))
 print(samples_DESI[2].getInlineLatex('mnu',limit=2))
 
+# %% [markdown]
+# #### Constraints on nrun
+
 # %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_nrun
+
+
+# new_samples=cmb_w_wa['samples'].copy()
+# new_samples=cmb_omega_k['samples'].copy()
+# new_samples=cmb_mnu['samples'].copy()
+# new_samples=cmb_nrun['samples'].copy()
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
+g = plots.getSubplotPlotter(width_inch=8)
+g.settings.axes_fontsize = 12
+g.settings.legend_fontsize = 14
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','nrun'],
+                legend_labels=["Planck 2018",labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+#plt.savefig(plot_label+'.pdf')
+#plt.savefig(plot_label+'.png')
+
+# %% [markdown]
+# #### Constraints on nrun and nrunrun
+
+# %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_nrun_nrunrun
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
+g = plots.getSubplotPlotter(width_inch=8)
+g.settings.axes_fontsize = 12
+g.settings.legend_fontsize = 14
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','nrun','nrunrun'],
+                legend_labels=["Planck 2018",labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+#plt.savefig(plot_label+'.pdf')
+#plt.savefig(plot_label+'.png')
+
+# %% [markdown]
+# ### Constraints on nrun_nnu_w_mnu
+
+# %%
+samples_DESI=[]
+true_DL2=[0.33,0.35,0.37]
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 2.3 # should it be 0.5?
+
+chain_type = cmb_nrun_nnu_w_mnu
+
+
+labels_DESI=[]
+for ii in range(len(true_DL2)):
+    new_samples=chain_type['samples'].copy()
+    p=new_samples.getParams()
+    new_loglike = coeff_mult * gaussian_chi2_mock_DESI(
+        p.linP_n_star, 
+        p.linP_DL2_star, 
+        true_DL2=true_DL2[ii],
+        neff_err=neff_err,
+        DL2_err=DL2_err,
+        r=r
+    )
+    new_samples.reweightAddingLogLikes(new_loglike) #re-weight cut_samples to account for the new likelihood
+    samples_DESI.append(new_samples)
+    labels_DESI.append(r'+ DESI Ly$\alpha \, \Delta^2_\ast = ${}'.format(true_DL2[ii]))
+
+
+# plikHM_TTTEEE_lowl_lowE_BAO_Riess18_Pantheon18_lensing
+g = plots.getSubplotPlotter(width_inch=8)
+g.settings.axes_fontsize = 12
+g.settings.legend_fontsize = 14
+g.triangle_plot([chain_type['samples'],samples_DESI[0],samples_DESI[1],samples_DESI[2]],
+                ['linP_DL2_star','linP_n_star','nrun','nnu','w','mnu'],
+                legend_labels=["Planck 2018 + SDSS + SN",labels_DESI[0],labels_DESI[1],labels_DESI[2]])
+#plt.savefig(plot_label+'.pdf')
+#plt.savefig(plot_label+'.png')
+
--- notebooks/planck/history_linP.py
+++ notebooks/planck/history_linP.py
@@ -8,9 +8,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.16.1
 #   kernelspec:
-#     display_name: cup1d
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: cup1d
+#     name: python3
 # ---
 
 # %% [markdown]
@@ -31,6 +31,7 @@ from cup1d.planck import planck_chains
 from cup1d.likelihood import marg_lya_like
 # because of black magic, getdist needs this strange order of imports
 # %matplotlib inline
+from cup1d.utils.utils import get_path_repo
 
 # %% [markdown]
 # ### Read extended CMB chains from historical releases
@@ -38,19 +39,29 @@ from cup1d.likelihood import marg_lya_like
 # These chains are already provided in cup1d, form different CMB releases. All chains have free neutrino mass.
 
 # %% jupyter={"outputs_hidden": false}
-model='base_mnu'
+# model='base_mnu'
+model='base'
 # WMAP9
-wmap9=planck_chains.get_planck_2013(model=model,data='WMAP')
-wmap9['label']='WMAP 9'
-# Planck 2013
-planck2013=planck_chains.get_planck_2013(model=model)
-planck2013['label']='Planck 2013'
-# Planck 2015
-planck2015=planck_chains.get_planck_2015(model=model)
-planck2015['label']='Planck 2015'
+# wmap9=planck_chains.get_planck_2013(model=model,data='WMAP')
+# wmap9['label']='WMAP 9'
+# # Planck 2013
+# planck2013=planck_chains.get_planck_2013(model=model)
+# planck2013['label']='Planck 2013'
+# # Planck 2015
+# planck2015=planck_chains.get_planck_2015(model=model)
+# planck2015['label']='Planck 2015'
 # Planck 2018
-planck2018=planck_chains.get_planck_2018(model=model)
-planck2018['label']='Planck 2018'
+# planck2018=planck_chains.get_planck_2018(model=model)
+# planck2018['label']='Planck 2018'
+
+root_dir=os.path.join(get_path_repo("cup1d"), "data", "planck_linP_chains")
+cmb = planck_chains.get_planck_2018(
+    model='base',
+    data='plikHM_TTTEEE_lowl_lowE_linP',
+    root_dir=root_dir,
+    linP_tag=None
+)
+
 
 # %%
 # put everything together to pass to getdist
@@ -58,60 +69,195 @@ analyses=[wmap9,planck2013,planck2015,planck2018]
 samples=[analysis['samples'] for analysis in analyses]
 labels=[analysis['label'] for analysis in analyses]
 
+# %%
+
+# params=['w', 'wa']
+# g = plots.getSubplotPlotter(width_inch=14)
+# g.settings.axes_fontsize = 12
+# g.settings.legend_fontsize = 16
+# g.triangle_plot(cmbw['samples'],params)
+
 # %% jupyter={"outputs_hidden": false}
-# plot traditional parameters
-params=['omegach2','omegabh2','mnu','H0','tau','logA','ns']
-g = plots.getSubplotPlotter(width_inch=14)
-g.settings.axes_fontsize = 12
-g.settings.legend_fontsize = 16
-g.triangle_plot(samples,params,legend_labels=labels)
+# # plot traditional parameters
+# params=['omegach2','omegabh2','mnu','H0','tau','logA','ns']
+# g = plots.getSubplotPlotter(width_inch=14)
+# g.settings.axes_fontsize = 12
+# g.settings.legend_fontsize = 16
+# g.triangle_plot(samples,params,legend_labels=labels)
 
 # %% [markdown]
 # ### Plot linear power parameters
 
 # %% jupyter={"outputs_hidden": false}
-# plot traditional parameters
-params=['linP_DL2_star','linP_n_star','linP_alpha_star','linP_f_star','linP_g_star']
-g = plots.getSubplotPlotter(width_inch=14)
-g.settings.axes_fontsize = 12
-g.settings.legend_fontsize = 16
-g.triangle_plot(samples,params,legend_labels=labels)
+# # plot traditional parameters
+# params=['linP_DL2_star','linP_n_star','linP_alpha_star','linP_f_star','linP_g_star']
+# g = plots.getSubplotPlotter(width_inch=14)
+# g.settings.axes_fontsize = 12
+# g.settings.legend_fontsize = 16
+# g.triangle_plot(samples,params,legend_labels=labels)
 
 # %% jupyter={"outputs_hidden": false}
-# plot traditional parameters (including now Omega_m and m_nu)
-params=['linP_DL2_star','linP_n_star','linP_alpha_star','linP_f_star','linP_g_star','omegam','mnu']
-g = plots.getSubplotPlotter(width_inch=14)
-g.settings.axes_fontsize = 12
-g.settings.legend_fontsize = 16
-g.triangle_plot(samples,params,legend_labels=labels)
+# # plot traditional parameters (including now Omega_m and m_nu)
+# params=['linP_DL2_star','linP_n_star','linP_alpha_star','linP_f_star','linP_g_star','omegam','mnu']
+# g = plots.getSubplotPlotter(width_inch=14)
+# g.settings.axes_fontsize = 12
+# g.settings.legend_fontsize = 16
+# g.triangle_plot(samples,params,legend_labels=labels)
 
 # %% [markdown]
 # ### Plot linear power parameters from chain and from Lya likelihoods
 
 # %%
+true_DL2=0.35
+true_neff=-2.3
+
+DL2_err = 0.056
+neff_err = 0.022
+r=-0.134
+coeff_mult = 1
+
 # create grid (note j in number of elements, crazy python)
-thresholds = [2.30,6.17,11.8]
-neff_grid,DL2_grid = np.mgrid[-2.4:-2.2:100j, 0.2:0.5:100j]
-chi2_Mc2005=marg_lya_like.gaussian_chi2_McDonald2005(neff_grid,DL2_grid)
-chi2_PD2015=marg_lya_like.gaussian_chi2_PalanqueDelabrouille2015(neff_grid,DL2_grid)
-chi2_Ch2019=marg_lya_like.gaussian_chi2_Chabanier2019(neff_grid,DL2_grid)
+# thresholds = [2.30,6.17,11.8]
+thresholds = [2.30, 6.18]
+neff_grid, DL2_grid = np.mgrid[-2.4:-2.2:200j, 0.2:0.65:200j]
+chi2_Mc2005=coeff_mult * marg_lya_like.gaussian_chi2_McDonald2005(neff_grid,DL2_grid)
+chi2_PD2015=coeff_mult * marg_lya_like.gaussian_chi2_PalanqueDelabrouille2015(neff_grid,DL2_grid)
+chi2_Ch2019=coeff_mult * marg_lya_like.gaussian_chi2_Chabanier2019(neff_grid,DL2_grid)
+chi2_Wa2024=coeff_mult * marg_lya_like.gaussian_chi2_Walther2024(neff_grid,DL2_grid)
+
+
+coeff_mult = 2.3 # no idea why
+chi2_desi = coeff_mult * marg_lya_like.gaussian_chi2(neff_grid,DL2_grid, true_neff, true_DL2, neff_err, DL2_err, r)
+
+
+# %%
+def print_err(ell):
+    ver = ell.collections[0].get_paths()[0].vertices
+    xcen = ver[:,0].mean()
+    ycen = ver[:,1].mean()
+    xerr = 0.5 * (ver[:,0].max() - ver[:,0].min())
+    yerr = 0.5 * (ver[:,1].max() - ver[:,1].min())
+    print(np.round(xerr, 3), np.round(yerr, 3))
+    return xcen, ycen, xerr, yerr
+
+# %%
+
+# g = plots.getSinglePlotter(width_inch=8)
+# g.plot_2d(cmb, ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25, 0.2,0.5])
+
+
+g = plots.getSubplotPlotter(width_inch=8)
+g.settings.axes_fontsize = 10
+g.settings.legend_fontsize = 14
+g.triangle_plot([cmb['samples']],
+                ['linP_n_star', 'linP_DL2_star'],
+                legend_labels=[r'$\Lambda$CDM'])
+
+# %%
+import scipy.stats as stats
+import matplotlib.lines as mlines
+
+# %%
+cmb.keys()
 
 # %% jupyter={"outputs_hidden": false}
+labs = ['McDonald+05', 'Palanque-Delabrouille+15', 'Chabanier+19', 'Walther+24', 'This work']
+
 # specify CMB chain to plot
-cmb=wmap9
-g = plots.getSinglePlotter(width_inch=8)
-#g.plot_2d(planck2018['samples'], ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25,0.2,0.5])
-g.plot_2d(cmb['samples'], ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25,0.2,0.5])
+# cmb=wmap9
+# g = plots.getSinglePlotter(width_inch=8)
+# g.plot_2d(cmb, ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25, 0.2,0.5])
+# g.plot_2d(cmb['samples'], ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25,0.2,0.5])
 #g.plot_2d(planck2018_mnu_BAO['samples'], ['linP_n_star', 'linP_DL2_star'],lims=[-2.4,-2.25,0.2,0.5])
-plt.contour(neff_grid,DL2_grid,chi2_Mc2005,levels=thresholds[:2],colors='green')
-plt.contour(neff_grid,DL2_grid,chi2_PD2015,levels=thresholds[:2],colors='red')
-plt.contour(neff_grid,DL2_grid,chi2_Ch2019,levels=thresholds[:2],colors='blue')
-plt.axhline(y=1,color='green',label='McDonald 2005')
-plt.axhline(y=1,color='red',label='Palanque-Delabrouille 2015')
-plt.axhline(y=1,color='blue',label='Chabanier 2019')
-plt.axhline(y=1,color='black',label=cmb['label'])
-plt.title(r'Linear power constraints at ($z=3$, $k_p=0.009$ s/km)')
-plt.grid()  
-plt.legend(loc=4)
+
+g = plots.getSubplotPlotter(width_inch=8)
+g.settings.axes_fontsize = 10
+g.settings.legend_fontsize = 14
+g.triangle_plot(
+    [cmb['samples']],
+    ['linP_n_star', 'linP_DL2_star'],
+    # legend_labels=[r'Planck+18 $\Lambda$CDM']
+)
+
+ax = g.subplots[1, 0]
+ax_NS = g.subplots[0, 0]
+ax_D2S = g.subplots[1, 1]
+x_D2S = np.linspace(0.2, 0.5, 500)
+x_NS = np.linspace(-2.4, -2.25, 500)
+
+CS = ax.contour(neff_grid,DL2_grid,chi2_Mc2005,levels=thresholds[:2],colors='C0')
+xcen, ycen, xerr, yerr = print_err(CS)
+pdf = stats.norm.pdf(x_D2S, ycen, yerr)
+ax_D2S.plot(x_D2S, pdf/pdf.max())
+pdf = stats.norm.pdf(x_NS, xcen, xerr)
+ax_NS.plot(x_NS, pdf/pdf.max())
+
+CS = ax.contour(neff_grid,DL2_grid,chi2_PD2015,levels=thresholds[:2],colors='C1')
+xcen, ycen, xerr, yerr = print_err(CS)
+pdf = stats.norm.pdf(x_D2S, ycen, yerr)
+ax_D2S.plot(x_D2S, pdf/pdf.max())
+pdf = stats.norm.pdf(x_NS, xcen, xerr)
+ax_NS.plot(x_NS, pdf/pdf.max())
+
+CS = ax.contour(neff_grid,DL2_grid,chi2_Ch2019,levels=thresholds[:2],colors='C2')
+xcen, ycen, xerr, yerr = print_err(CS)
+pdf = stats.norm.pdf(x_D2S, ycen, yerr)
+ax_D2S.plot(x_D2S, pdf/pdf.max())
+pdf = stats.norm.pdf(x_NS, xcen, xerr)
+ax_NS.plot(x_NS, pdf/pdf.max())
+
+CS = ax.contour(neff_grid,DL2_grid,chi2_Wa2024,levels=thresholds[:2],colors='C3')
+xcen, ycen, xerr, yerr = print_err(CS)
+pdf = stats.norm.pdf(x_D2S, ycen, yerr)
+ax_D2S.plot(x_D2S, pdf/pdf.max())
+pdf = stats.norm.pdf(x_NS, xcen, xerr)
+ax_NS.plot(x_NS, pdf/pdf.max())
+
+CS = ax.contour(neff_grid,DL2_grid,chi2_desi,levels=thresholds[:2],colors='C4')
+xcen, ycen, xerr, yerr = print_err(CS)
+pdf = stats.norm.pdf(x_D2S, ycen, yerr)
+ax_D2S.plot(x_D2S, pdf/pdf.max())
+pdf = stats.norm.pdf(x_NS, xcen, xerr)
+ax_NS.plot(x_NS, pdf/pdf.max())
+
+# plt.axhline(y=1,color='C0',label=cmb['label'])
+for ii in range(len(labs)):
+    ax.axhline(y=1,color='C'+str(ii),label=labs[ii])
+
+ax.set_xlim(-2.4, -2.25)
+ax.set_ylim(0.2, 0.5)
+
+# plt.title(r'Linear power constraints at ($z=3$, $k_p=0.009$ s/km)')
+# plt.grid()  
+
+# ax.legend()
+
+ # $\Lambda$CDM
+handles = []
+handles.append(mlines.Line2D([], [], color='black', label=r'Planck+18', lw=2))
+for ii in range(len(labs)):
+    handles.append(mlines.Line2D([], [], color='C'+str(ii), label=labs[ii], lw=2))
+    
+g.subplots[1, 1].legend(
+    handles=handles, 
+    bbox_to_anchor=(1, 2),
+    loc='upper right', 
+    borderaxespad=0.,
+    fontsize=14
+)
+
+
+# %%
+
+CS = plt.contour(DL2_grid, neff_grid, chi2_Ch2019, levels=thresholds[:2],colors='C0')
+print_err(CS)
+
+# plt.ylim(-2.41, -2.29)
+# plt.ylim(-2.4, -2.25)
+plt.ylim(-2.35, -2.33)
+# plt.xlim(0.14, 0.44)
+# plt.xlim(0.20, 0.40)
+plt.xlim(0.28, 0.36)
+plt.grid()
 
 # %%
