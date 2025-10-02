3cc5924a4c8e7ee3843a848ee7f5c7e24f70bde6
jchavesmontero <jchaves@ifae.es>
2025-10-02 23:51:27 +0200

    add data sampler

---
+++ scripts/data/data_sampler.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ["OMP_NUM_THREADS"] = "1"  # export OMP_NUM_THREADS=4
+import numpy as np
+from mpi4py import MPI
+from cup1d.likelihood.input_pipeline import Args
+from cup1d.likelihood.pipeline import Pipeline
+from cup1d.utils.utils import get_path_repo
+from cup1d.plots.plots_corner import plots_chain
+
+
+def main():
+    ## MPI stuff
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    emu = "mpg"
+    data_label = "DESIY1_QMLE3"
+    name_variation = sys.argv[1]
+
+    if name_variation == "nyx":
+        name_variation = None
+        emu = "nyx"
+    elif name_variation == "DESIY1_QMLE":
+        name_variation = None
+        data_label = "DESIY1_QMLE"
+    elif name_variation == "DESIY1_FFT3_dir":
+        name_variation = None
+        data_label = "DESIY1_FFT3_dir"
+    elif name_variation == "DESIY1_FFT_dir":
+        name_variation = None
+        data_label = "DESIY1_FFT_dir"
+    elif name_variation == "None":
+        name_variation = None
+
+    args = Args(
+        data_label=data_label,
+        emulator_label="CH24_" + emu + "cen_gpr",
+    )
+    args.set_baseline(
+        fit_type="global_opt",
+        fix_cosmo=True,
+        P1D_type=data_label,
+        name_variation=name_variation,
+        # mcmc_conf="explore",
+        mcmc_conf="test",
+    )
+    pip = Pipeline(args, out_folder=args.out_folder)
+    input_pars = pip.fitter.like.sampling_point_from_parameters().copy()
+
+    pip.run_minimizer(input_pars, restart=True)
+    pip.run_sampler()
+
+    if rank == 0:
+        plots_chain(
+            pip.fitter.save_directory, folder_out=pip.fitter.save_directory
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/data/launch_data_sampler.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH -A desi
+#SBATCH -q regular
+#SBATCH -t 06:00:00
+#SBATCH --constraint=cpu
+#SBATCH --nodes=1
+#SBATCH -J p1d
+#SBATCH -o logs/p1d.%j.out
+#SBATCH -e logs/p1d.%j.err
+#SBATCH --array=0-1   # number of variations minus 1
+
+echo "Job started at: $(date)"
+
+mkdir -p logs
+
+# Define variations
+
+# Define variations
+variations=(
+    "None"
+    "nyx"
+    # "DESIY1_QMLE"
+    # "DESIY1_FFT3_dir"
+    # "DESIY1_FFT_dir"
+    # "no_inflate"
+    # "no_emu_cov"
+    # "no_inflate_no_emu_cov"
+    # "cosmo"
+    # "cosmo_low"
+    # "cosmo_high"
+    # "metal_trad"
+    # "metal_si2"
+    # "metal_deco"
+    # "no_res"
+    # "more_igm"
+    # "less_igm"
+    # "metals_z"
+    # "hcd_z"
+    # "zmin"
+    # "zmax"
+    # "Turner24"
+    # "metal_thin"
+)
+
+source /global/homes/j/jjchaves/miniconda3/bin/activate lace
+
+# Pick variation for this array task
+var=${variations[$SLURM_ARRAY_TASK_ID]}
+echo "Job $SLURM_ARRAY_JOB_ID, task $SLURM_ARRAY_TASK_ID: running variation = $var"
+
+time srun -n 128 --unbuffered python /global/homes/j/jjchaves/cup1d/scripts/data/data_sampler.py $var
+
+echo "Job finished at: $(date)"
