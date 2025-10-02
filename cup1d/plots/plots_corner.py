8f050cca39d0db6112e2583caa1d02f350a2787c
jchavesmontero <jchaves@ifae.es>
2025-10-02 15:57:34 +0200

    new plot routine

--- cup1d/plots/plots_corner.py
+++ cup1d/plots/plots_corner.py
@@ -3,10 +3,14 @@ from corner import corner
 from emcee.autocorr import integrated_time
 import matplotlib.pyplot as plt
 
+from matplotlib.ticker import MaxNLocator
+
 from matplotlib import rcParams
 
 rcParams["mathtext.fontset"] = "stix"
 rcParams["font.family"] = "STIXGeneral"
+
+from cup1d.utils.various_dicts import param_dict
 
 
 def plots_chain(
@@ -122,19 +126,170 @@ def corner_chain(
 
 
 def corr_compressed(
-    dat, folder_out=None, nburn_extra=0, ftsize=20, truth=[0, 0], labels=None
+    dat,
+    labels,
+    priors,
+    folder_out=None,
+    nburn_extra=0,
+    ftsize=20,
+    truth=[0, 0],
+    sigmas=2,
 ):
-    # fig corr
-    for ii in range(2, ndim):
-        fig, ax = plt.subplots(2, sharex=True)
-        ax[0].hist2d(dat[:, ii], dat[:, 0])
-        ax[1].hist2d(dat[:, ii], dat[:, 1])
+    # labels = fdict["like"]["free_param_names"]
+    frange = 0.1
+    cmap = plt.get_cmap("Blues")
+    groups = ["tau", "sigT_kms", "gamma", "Lya", ["SiIIa", "SiIIb"], "HCD"]
+    egroups = ["cte", "cte", "cte", "exp", "exp", "exp"]
+    # groups = [["SiIIa", "SiIIb"]]
+    # egroups = ["exp"]
+    for igroup, key in enumerate(groups):
+        lab_use = {}
+        for ii, lab in enumerate(labels):
+            if len(key) == 2:
+                if (key[0] in lab) | (key[1] in lab):
+                    lab_use[lab] = ii
+            else:
+                if key in lab:
+                    lab_use[lab] = ii
 
-        ax[0].set_ylabel(r"$\Delta_\star$")
-        ax[1].set_ylabel(r"$\Delta_\star$")
-        ax[1].set_xlabel(labels[ii])
-        plt.savefig(fname_out + "cosmo_" + str(ii) + ".pdf")
-        plt.savefig(fname_out + "cosmo_" + str(ii) + ".png")
+        if key in ["tau", "sigT_kms", "gamma"]:
+            sharex = "all"
+        else:
+            sharex = "col"
+
+        fig, ax = plt.subplots(
+            2,
+            len(lab_use),
+            sharex="col",
+            sharey="row",
+            figsize=(len(lab_use) * 3, 8),
+        )
+        for ii, lab in enumerate(lab_use):
+            pp = dat[:, lab_use[lab]]
+
+            x, y, h, levels = get_contours(pp, dat[:, 0], sigmas=sigmas)
+            if egroups[igroup] == "cte":
+                xplot = x
+            else:
+                xplot = np.exp(x)
+                if lab.startswith("s_"):
+                    xplot = 1 / xplot
+            cs1 = ax[0, ii].contour(xplot, y, h, levels=levels, colors="k")
+            cs1 = ax[0, ii].contourf(xplot, y, h, levels=levels, cmap=cmap)
+
+            x, y, h, levels = get_contours(pp, dat[:, 1], sigmas=sigmas)
+            if egroups[igroup] == "cte":
+                xplot = x
+            else:
+                xplot = np.exp(x)
+                if lab.startswith("s_"):
+                    xplot = 1 / xplot
+            cs2 = ax[1, ii].contour(xplot, y, h, levels=levels, colors="k")
+            cs2 = ax[1, ii].contourf(xplot, y, h, levels=levels, cmap=cmap)
+
+            ##
+            x_line = []
+            y_line = []
+            for collection in cs1.collections:
+                for path in collection.get_paths():
+                    v = path.vertices
+                    x_line.extend(v[:, 0])
+                    y_line.extend(v[:, 1])
+
+            if ii == 0:
+                yrange1 = [np.min(y_line), np.max(y_line)]
+                change_alims = True
+                change_blims = True
+            else:
+                if np.min(y_line) < yrange1[0]:
+                    yrange1[0] = np.min(y_line)
+                    change_alims = True
+                else:
+                    change_alims = False
+                if np.max(y_line) > yrange1[1]:
+                    yrange1[1] = np.max(y_line)
+                    change_blims = True
+                else:
+                    change_blims = False
+            if change_alims | change_blims:
+                dy = yrange1[1] - yrange1[0]
+                if change_alims:
+                    yrange1[0] -= frange * dy
+                if change_blims:
+                    yrange1[1] += frange * dy
+            ##
+
+            ##
+            x_line = []
+            y_line = []
+            for collection in cs2.collections:
+                for path in collection.get_paths():
+                    v = path.vertices
+                    x_line.extend(v[:, 0])
+                    y_line.extend(v[:, 1])
+
+            if ii == 0:
+                yrange2 = [np.min(y_line), np.max(y_line)]
+                change_alims = True
+                change_blims = True
+            else:
+                if np.min(y_line) < yrange2[0]:
+                    yrange2[0] = np.min(y_line)
+                    change_alims = True
+                else:
+                    change_alims = False
+                if np.max(y_line) > yrange2[1]:
+                    yrange2[1] = np.max(y_line)
+                    change_blims = True
+                else:
+                    change_blims = False
+            if change_alims | change_blims:
+                dy = yrange2[1] - yrange2[0]
+                if change_alims:
+                    yrange2[0] -= frange * dy
+                if change_blims:
+                    yrange2[1] += frange * dy
+            ##
+
+            xrange = [np.min(x_line), np.max(x_line)]
+            dy = xrange[1] - xrange[0]
+            xrange[0] -= frange * dy
+            xrange[1] += frange * dy
+
+            # if sigmas == 2:
+            #     range_par = np.percentile(dat[:, lab_use[lab]], [0.1, 99.9])
+            # else:
+            #     range_par = np.percentile(dat[:, lab_use[lab]], [3, 97])
+            ax[1, ii].set_xlabel(param_dict[lab], fontsize=ftsize + 2)
+
+            for i0 in range(2):
+                ax[i0, ii].set_xlim(xrange)
+                ax[i0, ii].xaxis.set_major_locator(
+                    MaxNLocator(nbins=3, prune=None)
+                )
+                ax[i0, ii].tick_params(
+                    axis="both", which="major", labelsize=ftsize
+                )
+                for j0 in range(2):
+                    if egroups[igroup] == "cte":
+                        xplot = priors[lab_use[lab], j0]
+                    else:
+                        xplot = np.exp(priors[lab_use[lab], j0])
+                        if lab.startswith("s_"):
+                            xplot = 1 / xplot
+                    ax[i0, ii].axvline(xplot, ls=":", color="k")
+
+        ax[0, 0].set_ylim(yrange1)
+        ax[1, 0].set_ylim(yrange2)
+        ax[0, 0].set_ylabel(r"$\Delta_\star$", fontsize=ftsize + 2)
+        ax[1, 0].set_ylabel(r"$n_\star$", fontsize=ftsize + 2)
+
+        plt.tight_layout()
+        if folder_out is None:
+            plt.show()
+        else:
+            plt.savefig(folder_out + "corr_compressed_" + str(igroup) + ".pdf")
+            plt.savefig(folder_out + "corr_compressed_" + str(igroup) + ".png")
 
     return
 
@@ -144,3 +299,43 @@ def plot_corr(chain):
 
     plt.imshow(mat, cmap="turbo")
     plt.colorbar()
+
+
+def get_contours(x, y, sigmas=1, bins=30):
+    """
+    Return mesh (X,Y), histogram values H (shape matches X,Y), and contour
+    thresholds that enclose 68% and optionally 95% of the samples.
+
+    Usage:
+        X, Y, H, levels = get_contours(x, y, sigmas=2, bins=50)
+        plt.contour(X, Y, H, levels=levels)
+    """
+    # Use raw counts (simpler to work with probability mass)
+    H, xedges, yedges = np.histogram2d(x, y, bins=bins, density=False)
+
+    # bin centres for plotting (meshgrid expects shape (ny, nx))
+    xcenters = 0.5 * (xedges[:-1] + xedges[1:])
+    ycenters = 0.5 * (yedges[:-1] + yedges[1:])
+    X, Y = np.meshgrid(xcenters, ycenters)
+
+    # flatten and sort counts descending
+    Hflat = H.flatten()
+    idx = np.argsort(Hflat)[::-1]
+    Hsorted = Hflat[idx]
+
+    # cumulative probability (mass)
+    cumsum = np.cumsum(Hsorted)
+    cumsum = cumsum / cumsum[-1]
+
+    probs = [0, 0.68] if sigmas == 1 else [0, 0.68, 0.95]
+
+    levels = []
+    for p in probs:
+        k = np.searchsorted(cumsum, p)  # first index where cumsum >= p
+        k = min(k, len(Hsorted) - 1)
+        levels.append(Hsorted[k])
+
+    levels = np.sort(levels)  # matplotlib requires ascending levels
+
+    # Return H transposed so it matches X, Y shapes for plt.contour(X, Y, H, ...)
+    return X, Y, H.T, levels
--- cup1d/utils/various_dicts.py
+++ cup1d/utils/various_dicts.py
@@ -1,39 +1,45 @@
 ## Dictionary to convert likelihood parameters into latex strings
 param_dict = {
-    "Delta2_p": "$\Delta^2_p$",
-    "mF": "$F$",
-    "gamma": "$\gamma$",
-    "sigT_Mpc": "$\sigma_T$",
-    "kF_Mpc": "$k_F$",
-    "n_p": "$n_p$",
-    "Delta2_star": "$\Delta^2_\star$",
-    "n_star": "$n_\star$",
-    "alpha_star": "$\\alpha_\star$",
-    "g_star": "$g_\star$",
-    "f_star": "$f_\star$",
-    "H0": "$H_0$",
-    "mnu": "$\Sigma m_\\nu$",
-    "As": "$A_s$",
-    "ns": "$n_s$",
-    "nrun": "$n_\mathrm{run}$",
-    "ombh2": "$\omega_b$",
-    "omch2": "$\omega_c$",
-    "cosmomc_theta": "$\\theta_{MC}$",
+    "Delta2_p": r"$\Delta^2_p$",
+    "mF": r"$F$",
+    "gamma": r"$\gamma$",
+    "sigT_Mpc": r"$\sigma_T$",
+    "kF_Mpc": r"$k_F$",
+    "n_p": r"$n_p$",
+    "Delta2_star": r"$\Delta^2_\star$",
+    "n_star": r"$n_\star$",
+    "alpha_star": r"$\alpha_\star$",
+    "g_star": r"$g_\star$",
+    "f_star": r"$f_\star$",
+    "H0": r"$H_0$",
+    "mnu": r"$\Sigma m_\nu$",
+    "As": r"$A_s$",
+    "ns": r"$n_s$",
+    "nrun": r"$n_\mathrm{run}$",
+    "ombh2": r"$\omega_b$",
+    "omch2": r"$\omega_c$",
+    "cosmomc_theta": r"$\theta_{MC}$",
 }
 
+hcd_dict = ["LLS", "sub-DLA", "small DLA", "large DLA"]
+
 for ii in range(11):
-    param_dict["tau_eff_" + str(ii)] = "$\tau_{\rm eff_" + str(ii) + "}$"
-    param_dict["sigT_kms_" + str(ii)] = "$\sigma_{\rm T_" + str(ii) + "}$"
-    param_dict["gamma_" + str(ii)] = "$\gamma_" + str(ii) + "$"
-    param_dict["kF_kms_" + str(ii)] = "$k_F_" + str(ii) + "$"
-    param_dict["R_coeff_" + str(ii)] = "$R_" + str(ii) + "$"
-    param_dict["ln_SN_" + str(ii)] = "$\log \mathrm{SN}_" + str(ii) + "$"
-    param_dict["ln_AGN_" + str(ii)] = "$\log \mathrm{AGN}_" + str(ii) + "$"
+    param_dict["tau_eff_" + str(ii)] = r"$f_{\tau_{\rm eff_" + str(ii) + "}}$"
+    param_dict["sigT_kms_" + str(ii)] = r"$f_{\sigma_{\rm T_" + str(ii) + "}}$"
+    param_dict["gamma_" + str(ii)] = r"$f_{\gamma_" + str(ii) + r"}$"
+    param_dict["kF_kms_" + str(ii)] = r"$f_{{{k_F}_" + str(ii) + r"}}$"
+    param_dict["R_coeff_" + str(ii)] = r"$R_" + str(ii) + r"$"
+    param_dict["ln_SN_" + str(ii)] = r"$\log \mathrm{SN}_" + str(ii) + r"$"
+    param_dict["ln_AGN_" + str(ii)] = r"$\log \mathrm{AGN}_" + str(ii) + r"$"
     for jj in range(1, 5):
         param_dict["HCD_damp" + str(jj) + "_" + str(ii)] = (
-            "$f_{\rm HCD" + str(jj) + "}_" + str(ii) + "$"
+            r"$f^\mathrm{HCD}_{{\rm "
+            + hcd_dict[jj - 1]
+            + "}_"
+            + str(ii)
+            + r"}$"
         )
-    param_dict["HCD_const_" + str(ii)] = "$c_{\rm HCD}_" + str(ii) + "$"
+    param_dict["HCD_const_" + str(ii)] = r"$c_{{\rm HCD}_" + str(ii) + r"}$"
 
 
 metal_lines = [
@@ -45,39 +51,67 @@ metal_lines = [
     "CIVa_CIVb",
     "MgIIa_MgIIb",
 ]
+metal_lines_latex_out = {
+    "Lya_SiIII": r"$\mathrm{Ly}\alpha-\mathrm{SiIII}$",
+    "Lya_SiII": r"$\mathrm{Ly}\alpha-\mathrm{SiII}$",
+    "Lya_SiIIa": r"$\mathrm{Ly}\alpha-\mathrm{SiIIa}$",
+    "Lya_SiIIb": r"$\mathrm{Ly}\alpha-\mathrm{SiIIb}$",
+    "Lya_SiIIc": r"$\mathrm{Ly}\alpha-\mathrm{SiIIc}$",
+    "SiIIa_SiIIb": r"$\mathrm{SiIIa}_\mathrm{SiIIb}$",
+    "SiIIa_SiIII": r"$\mathrm{SiIIa}_\mathrm{SiIII}$",
+    "SiIIb_SiIII": r"$\mathrm{SiIIb}_\mathrm{SiIII}$",
+    "SiII_SiIII": r"$\mathrm{SiII}_\mathrm{SiIII}$",
+    "SiIIc_SiIII": r"$\mathrm{SiIIc}_\mathrm{SiIII}$",
+    "CIVa_CIVb": r"$\mathrm{CIVa}_\mathrm{CIVb}$",
+    "MgIIa_MgIIb": r"$\mathrm{MgIIa}_\mathrm{MgIIb}$",
+}
 metal_lines_latex = {
-    "Lya_SiIII": "$\mathrm{Ly}\alpha-\mathrm{SiIII}$",
-    "Lya_SiII": "$\mathrm{Ly}\alpha-\mathrm{SiII}$",
-    "Lya_SiIIa": "$\mathrm{Ly}\alpha-\mathrm{SiIIa}$",
-    "Lya_SiIIb": "$\mathrm{Ly}\alpha-\mathrm{SiIIb}$",
-    "Lya_SiIIc": "$\mathrm{Ly}\alpha-\mathrm{SiIIc}$",
-    "SiIIa_SiIIb": "$\mathrm{SiIIa}_\mathrm{SiIIb}$",
-    "SiIIa_SiIII": "$\mathrm{SiIIa}_\mathrm{SiIII}$",
-    "SiIIb_SiIII": "$\mathrm{SiIIb}_\mathrm{SiIII}$",
-    "SiII_SiIII": "$\mathrm{SiII}_\mathrm{SiIII}$",
-    "SiIIc_SiIII": "$\mathrm{SiIIc}_\mathrm{SiIII}$",
-    "CIVa_CIVb": "$\mathrm{CIVa}_\mathrm{CIVb}$",
-    "MgIIa_MgIIb": "$\mathrm{MgIIa}_\mathrm{MgIIb}$",
+    "Lya_SiIII": r"\mathrm{Ly}\alpha-\mathrm{SiIII}",
+    "Lya_SiII": r"\mathrm{Ly}\alpha-\mathrm{SiII}",
+    "Lya_SiIIa": r"\mathrm{Ly}\alpha-\mathrm{SiIIa}",
+    "Lya_SiIIb": r"\mathrm{Ly}\alpha-\mathrm{SiIIb}",
+    "Lya_SiIIc": r"\mathrm{Ly}\alpha-\mathrm{SiIIc}",
+    "SiIIa_SiIIb": r"\mathrm{SiII}-\mathrm{SiII}",
+    # "SiIIa_SiIII": r"\mathrm{SiIIa}-\mathrm{SiIII}",
+    # "SiIIb_SiIII": r"\mathrm{SiIIb}-\mathrm{SiIII}",
+    "SiIIa_SiIII": r"\mathrm{SiIIa}",
+    "SiIIb_SiIII": r"\mathrm{SiII}-\mathrm{SiIII}",
+    "SiII_SiIII": r"\mathrm{SiII}-\mathrm{SiIII}",
+    "SiIIc_SiIII": r"\mathrm{SiIIc}-\mathrm{SiIII}",
+    "CIVa_CIVb": r"\mathrm{CIVa}-\mathrm{CIVb}",
+    "MgIIa_MgIIb": r"\mathrm{MgIIa}-\mathrm{MgIIb}",
 }
 for metal_line in metal_lines:
     for ii in range(12):
-        param_dict["f_" + metal_line + "_" + str(ii)] = (
-            "$\mathrm{ln}\,f("
-            + metal_lines_latex[metal_line]
-            + "_"
-            + str(ii)
-            + ")$"
-        )
-        param_dict["s_" + metal_line + "_" + str(ii)] = (
-            "$\mathrm{ln}\,s("
-            + metal_lines_latex[metal_line]
-            + "_"
-            + str(ii)
-            + ")$"
-        )
-        param_dict["p_" + metal_line + "_" + str(ii)] = (
-            "$p(" + metal_lines_latex[metal_line] + "_" + str(ii) + ")$"
-        )
+        if (metal_line == "SiIIa_SiIII") | (metal_line == "SiIIb_SiIII"):
+            param_dict["f_" + metal_line + "_" + str(ii)] = (
+                r"$r_{{"
+                # r"$\log f_{{"
+                + metal_lines_latex[metal_line]
+                + "}_"
+                + str(ii)
+                + "}$"
+            )
+        else:
+            param_dict["f_" + metal_line + "_" + str(ii)] = (
+                r"$f_{{"
+                # r"$\log f_{{"
+                + metal_lines_latex[metal_line]
+                + "}_"
+                + str(ii)
+                + "}$"
+            )
+            param_dict["s_" + metal_line + "_" + str(ii)] = (
+                r"$k_{{"
+                # r"$\log s_{{"
+                + metal_lines_latex[metal_line]
+                + "}_"
+                + str(ii)
+                + "}$"
+            )
+        # param_dict["p_" + metal_line + "_" + str(ii)] = (
+        #     r"$p(" + metal_lines_latex[metal_line] + "_" + str(ii) + ")$"
+        # )
 
 param_dict_rev = {v: k for k, v in param_dict.items()}
 
@@ -102,12 +136,12 @@ cosmo_params = [
 
 ## list of strings for blobs
 blob_strings = [
-    "$\Delta^2_\star$",
-    "$n_\star$",
-    "$\\alpha_\star$",
-    "$f_\star$",
-    "$g_\star$",
-    "$H_0$",
+    r"$\Delta^2_\star$",
+    r"$n_\star$",
+    r"$\alpha_\star$",
+    r"$f_\star$",
+    r"$g_\star$",
+    r"$H_0$",
 ]
 blob_strings_orig = [
     "Delta2_star",
@@ -119,7 +153,7 @@ blob_strings_orig = [
 ]
 
 conv_strings = {
-    "Delta2_star": "$\Delta^2_\star$",
-    "n_star": "$n_\star$",
-    "alpha_star": "$\\alpha_\star$",
+    "Delta2_star": r"$\Delta^2_\star$",
+    "n_star": r"$n_\star$",
+    "alpha_star": r"$\alpha_\star$",
 }
