bebd44bce349aa80e84f20837e6747714f12ac9e
jchavesmontero <jchaves@ifae.es>
2026-03-19 18:42:06 +0100

    update notebook

--- notebooks/tutorials/dr1_tutorial.py
+++ notebooks/tutorials/dr1_tutorial.py
@@ -21,28 +21,133 @@
 # %autoreload 2
 
 import numpy as np
-import time, os, sys
+import os, sys
 import matplotlib.pyplot as plt
-
-# our own modules
-from lace.cosmo import camb_cosmo
-from lace.emulator.emulator_manager import set_emulator
-from cup1d.likelihood import lya_theory, likelihood
-from cup1d.likelihood.fitter import Fitter
-from cup1d.likelihood.plotter import Plotter
-
-from cup1d.likelihood.input_pipeline import Args
 from cup1d.likelihood.pipeline import Pipeline
 
-from cup1d.p1ds.data_DESIY1 import P1D_DESIY1
-from astropy.io import fits
 
+# %% [markdown]
+# ## Load P1D measurements and set likelihood
 
-from corner import corner
+# %%
+pip = Pipeline()
 
-from cup1d.utils.utils import get_path_repo
+# %% [markdown]
+# ## Plot P1D data 
 
-from scipy.stats import chi2 as chi2_scipy
-from cup1d.pipeline.set_archive import set_archive
+# %%
+pip.fitter.like.plot_p1d()
+
+# %%
+# measurements in z bins (no correlation between z bins)
+k_kms = pip.fitter.like.data.k_kms
+Pk_kms = pip.fitter.like.data.Pk_kms
+cov_Pk_kms = pip.fitter.like.cov_Pk_kms
+print(len(k_kms), len(Pk_kms), len(cov_Pk_kms))
+print(k_kms[0].shape, Pk_kms[0].shape, cov_Pk_kms[0].shape)
+
+# %%
+# measurements in full array (correlation between z bins)
+k_kms = pip.fitter.like.data.full_k_kms
+Pk_kms = pip.fitter.like.data.full_Pk_kms
+cov_Pk_kms = pip.fitter.like.full_cov_Pk_kms
+print(k_kms.shape, Pk_kms.shape, cov_Pk_kms.shape)
+
+# %% [markdown]
+# ### Components of the covariance matrix
+
+# %%
+pip.fitter.like.plot_cov_to_pk()
+
+# %%
+# access the components of the covariance matrix
+## stat + sys + emu
+cov_Pk_kms_tot = pip.fitter.like.cov_Pk_kms
+## stat
+cov_Pk_kms_stat = pip.fitter.like.data.covstat_Pk_kms
+## syst
+cov_Pk_kms_syst = []
+for ii in range(len(cov_Pk_kms_stat)):
+    cov_Pk_kms_syst.append(pip.fitter.like.data.cov_Pk_kms[ii] - cov_Pk_kms_stat[ii])
+## emu
+cov_Pk_kms_emu = pip.fitter.like.cov_emu_Pk_kms
+
+print(
+    len(cov_Pk_kms_tot), len(cov_Pk_kms_stat), len(cov_Pk_kms_syst), len(cov_Pk_kms_emu)
+)
+
+# %% [markdown]
+# ### Get predictions from the model
+
+# %%
+# list of model parameters
+
+for par in pip.fitter.like.free_params:
+    print(par.name, par.value, par.min_value, par.max_value)
+
+# %%
+# evaluate model for the initial value of the input parameters
+zs = pip.fitter.like.data.z
+k_kms = pip.fitter.like.data.k_kms
+ini_free_params = pip.fitter.like.free_params
+
+ini_model_Pk_kms = pip.fitter.like.theory.get_p1d_kms(
+    zs, k_kms, like_params=ini_free_params
+)[0]
+
+# %%
+# evaluate model for other values of input parameters, only changing As
+zs = pip.fitter.like.data.z
+k_kms = pip.fitter.like.data.k_kms
+
+new_free_params = []
+for par in pip.fitter.like.free_params:
+    old_value = par.value
+    new_par = par.get_new_parameter(0.5)
+    if par.name == "As":
+        # increase by 10%
+        new_par.value = old_value * 1.1
+    else:
+        # same value as before
+        new_par.value = old_value
+    new_free_params.append(new_par)
+
+new_As_model_Pk_kms = pip.fitter.like.theory.get_p1d_kms(
+    zs, k_kms, like_params=new_free_params
+)[0]
+
+# %% [markdown]
+# Show ratio of both predictions
+
+# %%
+ii = 0
+plt.plot(
+    k_kms[ii],
+    new_As_model_Pk_kms[ii] / ini_model_Pk_kms[ii],
+    label="z = " + str(zs[ii]),
+)
+plt.ylabel("$P_\mathrm{1D}(k)/P_\mathrm{1D}^\mathrm{ini}(k)$")
+plt.xlabel("$k_\parallel$ [km/s]")
+plt.legend()
+plt.show()
+
+# %% [markdown]
+# ### Compressed parameters
+
+# %%
+blob = pip.fitter.like.theory.get_blob_fixed_background(ini_free_params)
+ini_Delta2_star = blob[0]
+ini_n_star = blob[1]
+print(ini_Delta2_star, ini_n_star)
+
+# %%
+blob = pip.fitter.like.theory.get_blob_fixed_background(new_free_params)
+new_Delta2_star = blob[0]
+new_n_star = blob[1]
+print(new_Delta2_star, new_n_star)
+
+# %%
+# as expected, 10% larger value of the new parameter
+new_Delta2_star/ini_Delta2_star
 
 # %%
